34064b6b403c024ef5c61765a21f42816a4003dc
Nikhil <nikhilsaini746@gmail.com>
2024-05-28 12:58:02 +0530

    added some more code

--- script.js
+++ script.js
@@ -23,9 +23,40 @@
                    '','','',redCanon,redTank,redRicochet,redSemiricochet,redTitan ]
 
 var selectedBoxes = [];
+var gamestatus = true;
 var i; var c;
+
+function randomArrangement()
+{
+
+}
+
+function pauseGame()
+{
+   gamestatus = false;
+}
+
+function resumeGame()
+{
+   gamestatus = true;
+   
+}
+
+function resetGame()
+{
+   gamestatus = true;
+   randomArrangement();
+   player1time = 30;
+   player2time = 30;
+   populateGrid();
+}
+
  function populateGrid()
-{ 
+{   if(gamestatus==false)
+    {
+        alert("game paused");
+        return;
+    }
     const parent = document.getElementById('grid-Container');
     var h = 0;
     var c = 0;
@@ -73,8 +104,12 @@ var i; var c;
 
  var selectedBoxfound = false;
    function move(i)
- {  //var i =c;
-    //console.log(i);
+ {  if(gamestatus==false)
+    {
+        alert("game paused");
+        return;
+    }
+
     i = +(i-1);
     unlighten(selectedBoxes)
     selectedBoxes = [];
@@ -122,15 +157,11 @@ var i; var c;
 function lighten(selectedBoxes,i)
 {   
     console.log(selectedBoxes);
-    if(selectedBoxes.length!=0)
-        {
-            selectedBoxfound = true;
-        }
     selectedBoxes.map((val)=>{
         const child = document.getElementById('box-'+(val+1));
         child.classList.add('lighten');
          eventhandler = ()=>
-        {
+        {   
             selectedBoxClicked(i,val);
         }
         child.addEventListener("click",eventhandler)
@@ -138,7 +169,11 @@ function lighten(selectedBoxes,i)
 }
 
 function selectedBoxClicked(i,val)
-{  
+{  if(gamestatus==false)
+    {
+        alert("game paused");
+        return;
+    }
    console.log("interchanged");
    console.log(i+"  "+val); // working
    const child1 = document.getElementById('box-'+(i+1))
@@ -157,11 +192,13 @@ function selectedBoxClicked(i,val)
     {   console.log("hello");
         player1timer(0);
         player2timer(1);
+        shootBullet("red");
     }
     else if(boardStatus[val].indexOf("black")!=-1)
     {   console.log("pamello")
         player1timer(1);
-        player2timer(0)
+        player2timer(0);
+        shootBullet("black");
     }
    
    unlighten(selectedBoxes);
@@ -180,7 +217,9 @@ function unlighten(selectedBoxes)
 
 var x ;
 function player1timer(status)
-{ console.log(status)
+{ 
+console.log(status)
+
 //var countDownDate = new Date("Jan 5, 2027 15:37:31").getTime();
 var minutes = 0;
 
@@ -189,7 +228,7 @@ if(status==0)
         seconds = 30;
         document.getElementById("Player-1-timer").innerHTML = minutes + "m :" + seconds + "s ";
         player1time = 30;
-        console.log("status is 0-1")
+       // console.log("status is 0-1")
         clearInterval(x);
         return;
     }
@@ -200,17 +239,20 @@ if(status==0)
                 //var now = new Date().getTime();
               
                 //var distance = countDownDate - now;
-              
+                if(gamestatus==false)
+                    {
+                        player1time++;
+                    }
                 minutes = 0;
                 var seconds = player1time;
-                console.log("status is 1-1")
+             //   console.log("status is 1-1")
                 document.getElementById("Player-1-timer").innerHTML = minutes + "m :" + seconds + "s ";
                // console.log("player1 timer"+minutes+"  "+seconds);
                 
                 if (player1time < 0) {
                   clearInterval(x);
-                  document.getElementById("Player-2-timer").innerHTML = "PLAYER 2 WON";
-                  alert("Player 2 won");
+                  document.getElementById("Player-1-timer").innerHTML = "PLAYER 2 WON";
+                //  alert("Player 2 won");
                 }
                 player1time--;
               }, 1000);
@@ -228,7 +270,7 @@ function player2timer(status)
         document.getElementById("Player-2-timer").innerHTML = minutes + "m :" + seconds + "s ";
         clearInterval(y);
         player2time = 30;
-        console.log("status is 0-2")
+       // console.log("status is 0-2")
         return;
     }
     else if(status == 1)
@@ -236,9 +278,12 @@ function player2timer(status)
                 y = setInterval(function() {
 
                 //var now = new Date().getTime();
-                
+                if(gamestatus==false)
+                    {
+                        player2time++;
+                    }
                 //var distance = countDownDate - now;
-                console.log("status is 0-2")
+            //    console.log("status is 0-2")
                   minutes = 0;
                   var seconds = player2time;
                   
@@ -246,9 +291,9 @@ function player2timer(status)
                   //console.log("player2 timer"+minutes+"   "+seconds);
                   
                   if (player2time < 0) {
-                    clearInterval(x);
+                    clearInterval(y);
                     document.getElementById("Player-2-timer").innerHTML = "PLAYER 1 WON";
-                    alert("Player 1 won");
+                 //   alert("Player 1 won");
                   }
                   player2time--;
                 }, 1000);
@@ -256,17 +301,128 @@ function player2timer(status)
 
 }
 
-function shootBullet()
+function calculateDifferenceForCanon(color)
+{  var diff ; var index1; var index2; var ObjectHit = '';
+   if(color == "red")
+    {
+          index1 = boardStatus.indexOf(redCanon);
+          index2 = index1-8;
+         while(boardStatus[index2]=='' && index2>=0)
+            {
+                index2 = index2 - 8;
+            }
+            ObjectHit = boardStatus[index2];
+            diff = (index1-index2)/8;
+    }
+    else if(color == "black")
+        {
+             index1 = boardStatus.indexOf(blackCanon);
+             index2 = index1+8;
+            while(boardStatus[index2]=='' && index2<=63)
+               {
+                   index2 = index2 + 8;
+               }
+               ObjectHit = boardStatus[index2];
+               diff = (index2 - index1)/8;
+        }
+        return [index1,index2,diff,ObjectHit];
+}
+async function shootBullet(color)
+{
+   var [index1,index2,diff,ObjectHit] = calculateDifferenceForCanon(color);
+   var bullet = (color=="red")?('./assets/red-bullet.svg'):('./assets/black-bullet.svg');
+   const svg = document.createElement("img");
+   svg.setAttribute( 'width','80px');
+   svg.setAttribute( 'height','80px');
+   svg.setAttribute('src',bullet);
+   svg.setAttribute('style',' align-content: center; ');
+   if(color=="red")
+    {
+        index1 = index1-8;
+    
+    var box = document.getElementById('box-'+(index1+1));
+    box.appendChild(svg);
+    var x = setInterval(function(){
+        box.removeChild(svg);
+        console.log("bullet fired")
+        index1 = index1-8;
+        if(index1<index2)
+            {  hittingObject(ObjectHit);
+               clearInterval(x);
+            }
+      box = document.getElementById('box-'+(index1+1));
+      box.appendChild(svg);
+    },250)
+    }
+    else{
+        index1 = index1+8;
+        var box = document.getElementById('box-'+(index1+1));
+        box.appendChild(svg);
+        var x = setInterval(function(){
+            box.removeChild(svg);
+            console.log("bullet fired")
+            index1 = index1+8;
+            if(index1>index2)
+                {  hittingObject(ObjectHit);
+                   clearInterval(x);
+                }
+          box = document.getElementById('box-'+(index1+1));
+          box.appendChild(svg);
+        },250)
+    }
+    
+}
+
+function hittingObject(ObjectHit)
+{   // multiple hits through rico and semirico
+    // others == ignore or crash
+    console.log(ObjectHit);
+    if(ObjectHit.indexOf("titan")!=-1)
+        {
+            winningLogic(ObjectHit);
+        }
+}
+function winningLogic(ObjectHit)
+{
+   if(ObjectHit.indexOf("black")!=-1)
+    {
+        alert("red won");
+    }
+    else
+    {
+        alert("black won");
+    }
+}
+function makeSound()
 {
 
 }
-
-function winningLogic()
-{
-
-}
-
 function rotatePiece()
 {
 
 }
+
+function gameChanceController()
+{
+
+}
+
+function saveToStorage()
+{
+    
+}
+
+function undo()
+{
+
+}
+
+function redo()
+{
+
+}
+
+function displayStorage()
+{
+    
+} 
